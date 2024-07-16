e00f9003c33d389bdea3b30560fe600b13a29fa5
mingxue619 <mingxue619@gmail.com>
2024-07-16 17:07:03 +0800

    capacitor

--- build_capacitor/capacitor.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    appId: 'com.buddhaputra.cbeta.app',
-    appName: 'cbeta',
-    webDir: 'www'
-};
-exports.default = config;
