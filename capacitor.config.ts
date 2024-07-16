5f5a52a66eb2a08928a17e17639d59ec536af2a9
mingxue619 <mingxue619@gmail.com>
2024-07-16 16:17:58 +0800

    capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  // appId: 'io.ionic.starter',
   appId: 'com.buddhaputra.cbeta.app',
-  appName: '電子佛典',
+  appName: 'cbeta',
   webDir: 'www'
 };
 
